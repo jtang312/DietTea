31334ab48c28f0d0da75758ebac2319e958de4b6
Jonathan Tang <jtang312@gmail.com>
2022-04-16 19:30:15 -0400

    basic app config

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3030
+
+// middleware
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
